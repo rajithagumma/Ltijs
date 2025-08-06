55e3cfd63327f144c8b2a6f45fc48d528f6b1c5d
rajithagumma <gummarajitha123@gmail.com>
2025-08-06 17:33:52 +0530

    grading

--- index.js
+++ index.js
@@ -41,45 +41,45 @@ lti.onConnect((token, req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-// lti.app.post('/grade', async (req, res) => {
-//   try {
-//     const idtoken = res.locals.token // IdToken
-//     const score = req.body.grade // User numeric score sent in the body
-//     // Creating Grade object
-//     const gradeObj = {
-//       userId: idtoken.user,
-//       scoreGiven: score,
-//       scoreMaximum: 100,
-//       activityProgress: 'Completed',
-//       gradingProgress: 'FullyGraded'
-//     }
+lti.app.post('/grade', async (req, res) => {
+  try {
+    const idtoken = res.locals.token // IdToken
+    const score = req.body.grade // User numeric score sent in the body
+    // Creating Grade object
+    const gradeObj = {
+      userId: idtoken.user,
+      scoreGiven: score,
+      scoreMaximum: 100,
+      activityProgress: 'Completed',
+      gradingProgress: 'FullyGraded'
+    }
 
-//     // Selecting linetItem ID
-//     let lineItemId = idtoken.platformContext.endpoint.lineitem // Attempting to retrieve it from idtoken
-//     if (!lineItemId) {
-//       const response = await lti.Grade.getLineItems(idtoken, { resourceLinkId: true })
-//       const lineItems = response.lineItems
-//       if (lineItems.length === 0) {
-//         // Creating line item if there is none
-//         console.log('Creating new line item')
-//         const newLineItem = {
-//           scoreMaximum: 100,
-//           label: 'Grade',
-//           tag: 'grade',
-//           resourceLinkId: idtoken.platformContext.resource.id
-//         }
-//         const lineItem = await lti.Grade.createLineItem(idtoken, newLineItem)
-//         lineItemId = lineItem.id
-//       } else lineItemId = lineItems[0].id
-//     }
+    // Selecting linetItem ID
+    let lineItemId = idtoken.platformContext.endpoint.lineitem // Attempting to retrieve it from idtoken
+    if (!lineItemId) {
+      const response = await lti.Grade.getLineItems(idtoken, { resourceLinkId: true })
+      const lineItems = response.lineItems
+      if (lineItems.length === 0) {
+        // Creating line item if there is none
+        console.log('Creating new line item')
+        const newLineItem = {
+          scoreMaximum: 100,
+          label: 'Grade',
+          tag: 'grade',
+          resourceLinkId: idtoken.platformContext.resource.id
+        }
+        const lineItem = await lti.Grade.createLineItem(idtoken, newLineItem)
+        lineItemId = lineItem.id
+      } else lineItemId = lineItems[0].id
+    }
 
-//     // Sending Grade
-//     const responseGrade = await lti.Grade.submitScore(idtoken, lineItemId, gradeObj)
-//     return res.send(responseGrade)
-//   } catch (err) {
-//     return res.status(500).send({ err: err.message })
-//   }
-// })
+    // Sending Grade
+    const responseGrade = await lti.Grade.submitScore(idtoken, lineItemId, gradeObj)
+    return res.send(responseGrade)
+  } catch (err) {
+    return res.status(500).send({ err: err.message })
+  }
+})
 
 // 3. Deploy the provider first — this must come before registering platforms!
 await lti.deploy({ port: 3000 });
